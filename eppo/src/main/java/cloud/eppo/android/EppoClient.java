b1825fc6bf9d179ced2dfe8478173bb196798139
Eric Petzel <epetzel@gmail.com>
2022-10-06 18:58:47 +0200

    remove unused method

--- eppo/src/main/java/cloud/eppo/android/EppoClient.java
+++ eppo/src/main/java/cloud/eppo/android/EppoClient.java
@@ -45,22 +45,6 @@ public class EppoClient {
         requestor.load(callback);
     }
 
-    private String generateMD5Hash(String input) {
-        try {
-            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
-            digest.update(input.getBytes());
-            byte messageDigest[] = digest.digest();
-
-            StringBuffer hexString = new StringBuffer();
-            for (int i = 0; i < messageDigest.length; i++)
-                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     private EppoValue getSubjectVariationOverride(String subjectKey, FlagConfig flagConfig) {
         String subjectHash = Utils.getMD5Hex(subjectKey);
         if (flagConfig.getOverrides().containsKey(subjectHash)) {
