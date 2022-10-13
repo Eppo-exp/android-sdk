7ed257b38e8015540fe70af2ddcc1e3725ff4402
Eric Petzel <epetzel@gmail.com>
2022-10-13 16:33:52 -0600

    assignment logging

---
+++ eppo/src/main/java/cloud/eppo/android/logging/Assignment.java
@@ -0,0 +1,44 @@
+package cloud.eppo.android.logging;
+
+import cloud.eppo.android.dto.SubjectAttributes;
+
+public class Assignment {
+    private String experiment;
+    private String variation;
+    private String subject;
+    private String timestamp;
+    private SubjectAttributes subjectAttributes;
+
+    public Assignment(String experiment, String variation, String subject, String timestamp, SubjectAttributes subjectAttributes) {
+        this.experiment = experiment;
+        this.variation = variation;
+        this.subject = subject;
+        this.timestamp = timestamp;
+        this.subjectAttributes = subjectAttributes;
+    }
+
+    public String getExperiment() {
+        return experiment;
+    }
+
+    public String getVariation() {
+        return variation;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public SubjectAttributes getSubjectAttributes() {
+        return subjectAttributes;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject " + subject + " assigned to variation " + variation + " in experiment " + experiment;
+    }
+}
---
+++ eppo/src/main/java/cloud/eppo/android/logging/AssignmentLogger.java
@@ -0,0 +1,6 @@
+package cloud.eppo.android.logging;
+
+public interface AssignmentLogger {
+    void logAssignment(Assignment assignment);
+}
+
