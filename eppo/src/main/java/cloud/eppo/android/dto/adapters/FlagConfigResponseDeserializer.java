b4ceb9255a792adcee2f3f09f6903ec818fa96b6
Tyler Potter <tyler.potter@datadoghq.com>
2026-07-24 09:50:40 -0600

    fix: update to new SNAPSHOT API (ShardRange interface + 2-param ConfigurationParser)

--- eppo/src/main/java/cloud/eppo/android/dto/adapters/FlagConfigResponseDeserializer.java
+++ eppo/src/main/java/cloud/eppo/android/dto/adapters/FlagConfigResponseDeserializer.java
@@ -10,12 +10,12 @@ import cloud.eppo.api.dto.FlagConfig;
 import cloud.eppo.api.dto.FlagConfigResponse;
 import cloud.eppo.api.dto.OperatorType;
 import cloud.eppo.api.dto.Shard;
+import cloud.eppo.api.dto.ShardRange;
 import cloud.eppo.api.dto.Split;
 import cloud.eppo.api.dto.TargetingCondition;
 import cloud.eppo.api.dto.TargetingRule;
 import cloud.eppo.api.dto.Variation;
 import cloud.eppo.api.dto.VariationType;
-import cloud.eppo.model.ShardRange;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -219,7 +219,7 @@ public class FlagConfigResponseDeserializer extends StdDeserializer<FlagConfigRe
       for (JsonNode rangeNode : shardNode.get("ranges")) {
         int start = rangeNode.get("start").asInt();
         int end = rangeNode.get("end").asInt();
-        ranges.add(new ShardRange(start, end));
+        ranges.add(new ShardRange.Default(start, end));
       }
       shards.add(new Shard.Default(salt, ranges));
     }
--- example/src/main/java/cloud/eppo/androidexample/GsonConfigurationParser.java
+++ example/src/main/java/cloud/eppo/androidexample/GsonConfigurationParser.java
@@ -18,12 +18,12 @@ import cloud.eppo.api.dto.FlagConfig;
 import cloud.eppo.api.dto.FlagConfigResponse;
 import cloud.eppo.api.dto.OperatorType;
 import cloud.eppo.api.dto.Shard;
+import cloud.eppo.api.dto.ShardRange;
 import cloud.eppo.api.dto.Split;
 import cloud.eppo.api.dto.TargetingCondition;
 import cloud.eppo.api.dto.TargetingRule;
 import cloud.eppo.api.dto.Variation;
 import cloud.eppo.api.dto.VariationType;
-import cloud.eppo.model.ShardRange;
 import cloud.eppo.parser.ConfigurationParseException;
 import cloud.eppo.parser.ConfigurationParser;
 import com.google.gson.JsonArray;
@@ -292,7 +292,7 @@ public class GsonConfigurationParser implements ConfigurationParser<Configuratio
           JsonObject range = rangeElement.getAsJsonObject();
           int start = range.get("start").getAsInt();
           int end = range.get("end").getAsInt();
-          ranges.add(new ShardRange(start, end));
+          ranges.add(new ShardRange.Default(start, end));
         }
       }
       shards.add(new Shard.Default(salt, ranges));
